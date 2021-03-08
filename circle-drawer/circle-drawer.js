b7ee6be0a05153bc21c24fc64b154dda5931f29b
Daniel Berge <daniel@berge.us>
2021-03-08 15:04:00 +0100

    Move eventlistener to canvas

--- circle-drawer/circle-drawer.js
+++ circle-drawer/circle-drawer.js
@@ -2,11 +2,11 @@ let system = new hd.ConstraintSystem();
 
 
 window.onload = () => {
-    document.addEventListener('click', event => {
-        let canvas = document.getElementById("canvas");
+    let canvas = document.getElementById("canvas");
+    canvas.addEventListener('click', event => {
         let ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(event.x, event.y, 40, 0, 2 * Math.PI);
+        ctx.arc(event.x - 10, event.y - 100, 40, 0, 2 * Math.PI);
         ctx.stroke();
     })
 }
