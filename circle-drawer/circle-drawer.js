2d65067af102d5a5dc6aa887c17c2e8788c55281
Daniel Berge <daniel@berge.us>
2021-03-09 13:03:20 +0100

    Add slider to circle drawer

--- circle-drawer/circle-drawer.js
+++ circle-drawer/circle-drawer.js
@@ -3,13 +3,20 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const circles = [];
+let selectedCircleIndex;
+
+let adjust = document.getElementById("adjust");
+let slider = document.getElementById("slider");
 
 window.onload = () => {
     canvas.addEventListener('click', event => {
         let any = false;
         circles.forEach(circle => {
             if (ctx.isPointInPath(circle.path, event.offsetX, event.offsetY)) {
-                alert("Clicked");
+                adjust.style.display = "block";
+                slider.value = circle.radius;
+                selectedCircleIndex = circles.indexOf(circle);
+
                 any = true;
             }
         })
@@ -34,9 +41,36 @@ window.onload = () => {
             } else {
                 ctx.fillStyle = 'red';
             }
-
-            //ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.fill(circle.path);
         })
     });
+
+    let span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        adjust.style.display = "none";
+    }
+
+
+    slider.addEventListener('input', () => {
+        let obj = circles[selectedCircleIndex];
+        obj.radius = slider.value;
+        let path = new Path2D();
+        path.arc(obj.x, obj.y, slider.value, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red';
+        ctx.fill(path);
+        circles[selectedCircleIndex] = obj;
+    });
+}
+
+window.requestAnimationFrame(step);
+
+let start;
+
+function step(timestamp) {
+    console.log("fsf");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    circles.forEach(circle => {
+        ctx.fill(circle.path);
+    });
+
+    window.requestAnimationFrame(step);
 }
