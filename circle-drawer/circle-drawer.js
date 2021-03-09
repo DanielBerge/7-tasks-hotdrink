5aa335dbeb324edee69633c40d9a47c63668ccdf
Daniel Berge <daniel@berge.us>
2021-03-09 11:31:04 +0100

    Working example of circle drawing

--- circle-drawer/circle-drawer.js
+++ circle-drawer/circle-drawer.js
@@ -1,12 +1,42 @@
 let system = new hd.ConstraintSystem();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+const circles = [];
 
 window.onload = () => {
-    let canvas = document.getElementById("canvas");
     canvas.addEventListener('click', event => {
-        let ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(event.x - 10, event.y - 100, 40, 0, 2 * Math.PI);
-        ctx.stroke();
+        let any = false;
+        circles.forEach(circle => {
+            if (ctx.isPointInPath(circle.path, event.offsetX, event.offsetY)) {
+                alert("Clicked");
+                any = true;
+            }
+        })
+        if (!any) {
+            let path = new Path2D();
+            path.arc(event.x - 10, event.y - 100, 40, 0, 2 * Math.PI);
+            ctx.fillStyle = 'red';
+            ctx.fill(path);
+            circles.push({
+                x: event.x - 10,
+                y: event.y - 100,
+                radius: 40,
+                path: path,
+            })
+        }
     })
+
+    canvas.addEventListener('mousemove', function (event) {
+        circles.forEach(circle => {
+            if (ctx.isPointInPath(circle.path, event.offsetX, event.offsetY)) {
+                ctx.fillStyle = 'green';
+            } else {
+                ctx.fillStyle = 'red';
+            }
+
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fill(circle.path);
+        })
+    });
 }
