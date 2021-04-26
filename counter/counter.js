b2e3722b2977c21926086efcb8516b76255e9334
Daniel Berge <daniel@berge.us>
2021-04-26 11:30:43 +0200

    Make counter example use hotdrink

--- counter/counter.js
+++ counter/counter.js
@@ -1,9 +1,20 @@
+import {innerTextBinder} from "../packages/binders.js";
+
 let countElement = document.getElementById('count');
 let buttonElement = document.getElementById('button');
-let count = 0;
+
+let system = new hd.ConstraintSystem();
 
 window.onload = () => {
+    let comp = hd.component`
+        var count = 0;
+    `;
+
+    system.addComponent(comp);
+    system.update();
+
+    innerTextBinder(countElement, comp.vs.count);
     buttonElement.addEventListener('click', () => {
-        countElement.innerText = (++count).toString()
+        system.scheduleCommand([comp.vs.count.value], [comp.vs.count.value], (count) => ++count );
     });
 }
